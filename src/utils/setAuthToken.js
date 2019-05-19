8a8b2bddb322b6b48e92a13d05f25240bd2a7a84
dasitha <dasitha.sandaruwan@digiratina.com>
2019-05-19 23:38:09 +0530

    finish the log in part and tested

---
+++ src/utils/setAuthToken.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const setAuthToken = token =>{
+    if(token){
+        //apply for every req
+        axios.defaults.headers.common['Authorization'] = token;
+
+    }
+
+    else {
+        //delete auth header
+
+        delete axios.defaults.headers.common['Authorization'];
+    }
+}
+
+export default setAuthToken;
