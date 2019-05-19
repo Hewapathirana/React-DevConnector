c0dab7b83c3533acb3e6a8f7a7b13814c6e51765
dasitha <dasitha.sandaruwan@digiratina.com>
2019-05-19 12:57:57 +0530

    SET UP THE REDUX

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router,Route} from "react-router-dom";
-
-
+import {Provider} from 'react-redux';
+import store from './store'
 import './App.css';
 import Navbar from "./component/layout/Navbar";
 import Footer from "./component/layout/Footer";
@@ -9,9 +9,14 @@ import Landing from "./component/layout/Landing";
 import Register from "./auth/Register";
 import Login from "./auth/Login";
 
+
+
+
 class App extends Component{
   render() {
     return (
+
+        <Provider store={ store }>
         <Router>
         <div className="App">
 
@@ -25,6 +30,7 @@ class App extends Component{
           <Footer/>
         </div>
         </Router>
+        </Provider>
     )
   }
 }
